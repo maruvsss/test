d448049fe4c4671a218bf3bb52e2de9022325f55
maruv.sss <gurniksasha2001@icloud.com>
2023-07-26 18:44:17 +0300

    first commit

---
+++ test1.py
@@ -0,0 +1,18 @@
+#x=input("Your name:")
+#y=int(input("What your age:"))
+#print("Hello" , x +", your from",2023-y)
+#print(round(a)) Округляет число
+print("Maruv.sss Python 1.0\n")
+what=input("Что выполнять? + или - :")
+a=int(input("Первое число:"))
+b=int(input("Второе число:"))
+
+
+if what == "+":
+    c = a + b
+    print("Результат вычислений:" + str(c))
+elif what == "-":
+    c = a - b
+    print("Результат вычислений:" + str(c))
+else:
+    print("Выполнена неправильная операция!")
